86f40f6fe9ce1b72c1944eb901206cccdad363e9
Lilian VERLHAC <lilian.verlhac@outlook.com>
2021-12-06 13:37:37 +0000

    Add chackra

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import {Route, Routes} from "react-router-dom";
+import {ChakraProvider} from "@chakra-ui/react";
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" /*element={<PageComponent/>}*//>
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ChakraProvider>
+                <Routes>
+                    <Route path="/" /*element={<PageComponent/>}*//>
+                </Routes>
+            </ChakraProvider>
+        </div>
+    );
 }
 
 export default App;
