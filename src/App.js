61c4634f06b6e765648022b7dd15f4bc8d09bfec
Lilian VERLHAC <lilian.verlhac@outlook.com>
2021-12-06 16:00:32 +0000

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import {Route, Routes} from "react-router-dom";
 import {ChakraProvider} from "@chakra-ui/react";
+import theme from "./utils/theme";
+import HomePage from "./pages/HomePage";
+import ChangeCalculatorPage from "./pages/ChangeCalculatorPage";
+import ItemPackingPage from "./pages/ItemPackingPage";
+import ShoppingBasketPage from "./pages/ShoppingBasketPage";
+import StockPage from "./pages/StockPage";
+import NavBar from "./components/NavBar";
 
 function App() {
     return (
         <div className="App">
-            <ChakraProvider>
+            <ChakraProvider theme={theme}>
+                <NavBar/>
                 <Routes>
-                    <Route path="/" /*element={<PageComponent/>}*//>
+                    <Route path="/" exact element={<HomePage/>}/>
+                    <Route path="/stock" element={<StockPage/>}/>
+                    <Route path="/item-packing" element={<ItemPackingPage/>}/>
+                    <Route path="/shopping-basket" element={<ShoppingBasketPage/>}/>
+                    <Route path="/change" element={<ChangeCalculatorPage/>}/>
                 </Routes>
             </ChakraProvider>
         </div>
---
+++ src/pages/ChangeCalculatorPage.js
@@ -0,0 +1,3 @@
+export default function ChangeCalculatorPage() {
+    return <div/>
+}
