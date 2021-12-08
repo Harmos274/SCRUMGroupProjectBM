7785712165c1ade646e8b7ef75ce877475b1094e
Matthis CUSIN <matthis.cusin@epitech.eu>
2021-12-08 12:11:53 +0000

    [ADD] Size input filed component

---
+++ src/components/SizeInputField.js
@@ -0,0 +1,21 @@
+import {
+    NumberInput,
+    NumberInputField,
+    NumberInputStepper,
+    NumberIncrementStepper,
+    NumberDecrementStepper,
+} from '@chakra-ui/react'
+
+const SizeInputField = (props) => {
+    return (
+        <NumberInput {...props}>
+            <NumberInputField />
+            <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+            </NumberInputStepper>
+        </NumberInput>
+    )
+}
+
+export default SizeInputField;
