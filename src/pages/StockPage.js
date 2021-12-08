42f9150b95920b707b64627d68890a9d543254d0
Matthis CUSIN <matthis.cusin@epitech.eu>
2021-12-08 12:12:14 +0000

    [ADD] SizeInputField to stock page

--- src/pages/StockPage.js
+++ src/pages/StockPage.js
@@ -1,3 +1,40 @@
+import { Input, Flex } from '@chakra-ui/react'
+import SizeInputField from '../components/SizeInputField'
+import {
+    NumberInput,
+    NumberInputField,
+    NumberInputStepper,
+    NumberIncrementStepper,
+    NumberDecrementStepper,
+} from '@chakra-ui/react'
+
 export default function StockPage() {
-    return <div/>
+    return (
+        <div>
+            <Flex direction='column'>
+                {/*Name*/}
+                <Input/>
+                {/*Type*/}
+                <Input/>
+                {/*Quantity*/}
+                <NumberInput defaultValue={0} min={0}>
+                    <NumberInputField />
+                    <NumberInputStepper>
+                        <NumberIncrementStepper />
+                        <NumberDecrementStepper />
+                    </NumberInputStepper>
+                </NumberInput>
+                {/*Expiration Date*/}
+                <Input/>
+                <Flex>
+                    {/*Height*/}
+                    <SizeInputField defaultValue={0} min={0} precision={2} step={0.5}/>
+                    {/*Width*/}
+                    <Input/>
+                    {/*Length*/}
+                    <Input/>
+                </Flex>
+            </Flex>
+        </div>
+    )
 }
