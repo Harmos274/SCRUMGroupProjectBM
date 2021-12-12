4b3992da566439b93677e35060a9e04603a7c4bb
Brahimi <louis.brahimi@epitech.eu>
2021-12-12 18:10:11 +0100

    filter item

--- src/pages/ShoppingBasketPage.js
+++ src/pages/ShoppingBasketPage.js
@@ -3,6 +3,11 @@ import './ShoppingBasketPage.css';
 
 
 export default function ShoppingBasketPage() {
+    const [filtres, setFiltres] = useState({
+        Luxury: true,
+        Gift: false,
+        Essential: true
+    })
     const [listItem, setListItem] = useState([
         {
             name: "A",
@@ -78,7 +83,6 @@ export default function ShoppingBasketPage() {
     const [listItemBasket, setListItemBasket] = useState({});
 
     function addBasket(index) {
-        console.log("addBasket")
         if (typeof listItemBasket.index === "undefined") {
             listItemBasket.index = 1;
         } else {
@@ -90,29 +94,52 @@ export default function ShoppingBasketPage() {
         setListItem( newItem )
     }
 
+    function updateFiltre(filtre, value) {
+        const newFiltre = {... filtres}
+        switch (filtre) {
+            case "Luxury":
+                newFiltre.Luxury = value
+                break
+            case "Gift":
+                newFiltre.Gift = value
+                break
+            case "Essential":
+                newFiltre.Essential = value
+                break
+        }
+        setFiltres(newFiltre);
+    }
     return <div>
         <div className="conteneur-list-item">
+            <div className="filtre-item">
+                <label htmlFor="filtre1">Luxury</label><input type="checkbox" defaultChecked={filtres.Luxury} onChange={(e) => updateFiltre("Luxury", e.target.checked)}></input>
+                <label htmlFor="filtre2">Gift</label><input type="checkbox" defaultChecked={filtres.Gift} onChange={(e) => updateFiltre("Gift", e.target.checked)}></input>
+                <label htmlFor="filtre3">Essential</label><input type="checkbox" defaultChecked={filtres.Essential} onChange={(e) => updateFiltre("Essential", e.target.checked)}></input>
+            </div>
             <ul className="list-item">
-                {listItem.map(
-                    (item, index) =>
+                {listItem.map(function (item, index) {
+                    return (filtres[item.type] ?
                         <li className="item" key={index}>
                             <span className="type-item">{item.type}</span>
                             <div className="conteneur-item">
                                 <span className="description">Name: </span>
                                 <span className="info">{item.name}</span>
                             </div>
-                            
+
                             <div className="conteneur-item">
                                 <span className="description">Quantity available:</span>
                                 <span className="info">{item.quantity}</span>
                             </div>
                             <div className="conteneur-item">
                                 <span className="description">Dimensions:</span>
-                                <span className="info">({item.dimensions.length}/{item.dimensions.width}/{item.dimensions.height})</span>
+                                <span
+                                    className="info">({item.dimensions.length}/{item.dimensions.width}/{item.dimensions.height})</span>
                             </div>
                             <button className="button" onClick={() => addBasket(index)}>Add to basket</button>
-                        </li>
-                )}
+                        </li> : ''
+                    )
+                })
+                }
             </ul>
         </div>
     </div>;
