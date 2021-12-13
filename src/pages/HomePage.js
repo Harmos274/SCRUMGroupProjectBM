b477dd3b55bb90d74e0427246fbe449fbfa015f8
Lilian VERLHAC <lilian.verlhac@outlook.com>
2021-12-13 13:20:06 +0000

    Add styling to home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,19 @@
+import {Container, Image, Text} from "@chakra-ui/react";
+import BgUrl from "../assets/inventory-management-software-500x500.png"
+
 export default function HomePage() {
     return (
         <div>
+            <Container centerContent={true}>
+                <Image mt={"40px"} src={BgUrl}/>
+                <Text mt={"20px"} fontSize={"1.5em"}>
+                    Web service for simplified item stock management with several innovative exchange rate and item
+                    packing calculation systems
+                </Text>
+                <Text mt={"30px"}>
+                    Cedric Cepeda · Louis Brahimi · Matthis Cusin · Noemie Deraedt · Lilian Verlhac
+                </Text>
+            </Container>
         </div>
     );
 }
