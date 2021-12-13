428ddd3e75e114a25587071f3fb862517209120e
Noémie <noemie.deraedt@epitech.eu>
2021-12-13 15:13:42 +0100

    feat: 🎸 delete stock

--- src/pages/StockPage.js
+++ src/pages/StockPage.js
@@ -1,24 +1,40 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './StockPage.css'
-import {useBSMContext} from '../utils/store';
-import {Link} from "react-router-dom";
-import {Button, Flex} from "@chakra-ui/react";
+import { useBSMContext, removeItem } from '../utils/store';
+import { Link } from "react-router-dom";
+import { Button, Flex } from "@chakra-ui/react";
 
 function StockPage() {
-    const {items,} = useBSMContext();
+    const { items, dispatch } = useBSMContext();
     const [onlyExpired, setOnlyExpired] = useState(false)
-    const [displayedItems, setDisplayedItems] = useState(items)
+
+    const itemRenderer = (item) => {
+        return (
+            <div key={item.id} className="stock-item-element">
+                <div className="stock-page-item-info">{item.name}</div>
+                <div className="stock-page-item-info">{item.quantity}</div>
+                <div
+                    className="stock-page-item-info">{item.dimensions.height}x{item.dimensions.width}x{item.dimensions.length}cm
+                </div>
+                <div className="stock-page-item-info">{item.type}</div>
+                <div className="stock-page-item-info">€{getPrice[item.type]}</div>
+                <div className="stock-page-item-info">{new Date(item.expiration_date).toDateString()}</div>
+                <div className="stock-page-item-info">
+                    <button className="stock-page-delete-button" onClick={() => onClickDelete(item.id)}>Delete</button>
+                </div>
+            </div>
+        )
+    }
 
     function toggleFilter() {
-        if (onlyExpired) {
-            setDisplayedItems(items)
-        } else {
-            setDisplayedItems(items.filter(item => new Date().getTime() <= new Date(item.expiration_date).getTime()))
-        }
         setOnlyExpired(!onlyExpired)
     }
 
-    const getPrice = {"Luxury": 50, "Gift": 20, "Essential": 30};
+    function onClickDelete(index) {
+        dispatch(removeItem(index));
+    }
+
+    const getPrice = { "Luxury": 50, "Gift": 20, "Essential": 30 };
 
     return (
         <div id="stock-page-container">
@@ -29,7 +45,7 @@ function StockPage() {
                 </div>
                 <div id="stock-item-go-back">
                     <Button onClick={toggleFilter} h={"50%"} bg={"white"} border={"none"}
-                            variant={"unstyled"}>{onlyExpired ? 'Show all items' : 'Show only expired'}</Button>
+                        variant={"unstyled"}>{onlyExpired ? 'Show all items' : 'Show only expired'}</Button>
                 </div>
             </Flex>
 
@@ -43,23 +59,9 @@ function StockPage() {
                     <div className="stock-page-item-info">Expiration</div>
                     <div className="stock-page-item-info"></div>
                 </div>
-                {displayedItems.length !== 0 ? displayedItems.map((item, index) => {
-                        return (
-                            <div key={index} className="stock-item-element">
-                                <div className="stock-page-item-info">{item.name}</div>
-                                <div className="stock-page-item-info">{item.quantity}</div>
-                                <div
-                                    className="stock-page-item-info">{item.dimensions.height}x{item.dimensions.width}x{item.dimensions.length}cm
-                                </div>
-                                <div className="stock-page-item-info">{item.type}</div>
-                                <div className="stock-page-item-info">€{getPrice[item.type]}</div>
-                                <div className="stock-page-item-info">{new Date(item.expiration_date).toDateString()}</div>
-                                <div className="stock-page-item-info">
-                                    <button className="stock-page-delete-button">Delete</button>
-                                </div>
-                            </div>
-                        )
-                    }) :
+                {items.length !== 0 ?
+                    (onlyExpired ? items.filter(item => new Date().getTime() <= new Date(item.expiration_date).getTime()).map(itemRenderer)
+                    : items.map(itemRenderer)) :
                     <div>No items to display!</div>
                 }
             </div>
