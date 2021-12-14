3695a65085c7c4b765d07172848707b9b9f6aa47
Matthis CUSIN <matthis.cusin@epitech.eu>
2021-12-14 04:12:16 +0100

    [Add] Alert on add item [Update] Make all the fields mandatory

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -2,6 +2,10 @@ import { Input, Flex, Select, Button, Spacer } from '@chakra-ui/react'
 import { FormControl, FormLabel } from '@chakra-ui/react'
 import SizeInputField from './SizeInputField'
 import moment from 'moment';
+import {
+  Alert,
+  AlertIcon,
+} from '@chakra-ui/react'
 
 import {
   NumberInput,
@@ -14,31 +18,42 @@ import { useState } from 'react';
 
 const AddItem = (props) => {
 
-    const [name, setName] = useState('None');
-    const [type, setType] = useState('None');
-    const [quantity, setQuantity] = useState('0');
-    const [expirationDate, setExpirationDate] = useState('12/24/2030');
-    const [height, setHeight] = useState('.0');
-    const [length, setLength] = useState('.0');
-    const [width, setWidth] = useState('.0');
-    const [weight, setWeight] = useState('.0');
+    const [name, setName] = useState('');
+    const [type, setType] = useState('');
+    const [quantity, setQuantity] = useState('');
+    const [expirationDate, setExpirationDate] = useState('');
+    const [height, setHeight] = useState('');
+    const [length, setLength] = useState('');
+    const [width, setWidth] = useState('');
+    const [weight, setWeight] = useState('');
+    const [formError, setFormError] = useState(false);
+    const [formSuccess, setFormSuccess] = useState(false);
 
     var {onAddItem, ...other} = props;
     
     const addItemHandler = (e) => {
-      onAddItem(name, type, quantity, moment(expirationDate).toISOString(), height, length, width, weight);
+      if (name && type && quantity && expirationDate && height && length && width && weight) {
+        onAddItem(name, type, quantity, moment(expirationDate).toISOString(), height, length, width, weight);
+        setFormError(false)
+        setFormSuccess(true)
+        setTimeout(() => {
+          setFormSuccess(false);
+        }, 3500);
+      } else {
+        setFormError(true)
+      }  
     };
 
     return (
-        <Flex position="relative" direction='column' alignItems='center' maxWidth='500px' {...other}>
+        <Flex position="relative" direction='column' alignItems='center' maxWidth='505px' {...other}>
   
-          <FormControl id='item_name' mb="2%">
+          <FormControl id='item_name' mb="2%" isRequired>
             <FormLabel>Name</FormLabel>
             <Input placeholder="Christmas Tree, Candies..." type="text" onChange={e => setName(e.currentTarget.value)}/>
           </FormControl>
   
           <Flex flexDirection='row' width="100%" mb="2%">
-            <FormControl id='item_type' marginRight="10%">
+            <FormControl id='item_type' marginRight="10%" isRequired>
               <FormLabel>Type</FormLabel>
               <Select placeholder='Select type' onChange={e => setType(e.currentTarget.value)}>
                 <option value='Essential'   >Essential</option>
@@ -47,7 +62,7 @@ const AddItem = (props) => {
               </Select>
             </FormControl>
   
-            <FormControl id='item_quantity' mb="2%">
+            <FormControl id='item_quantity' mb="2%" isRequired>
               <FormLabel>Quantity</FormLabel>
               <NumberInput defaultValue={0} min={0} onChange={e => {setQuantity(e)}}>
                 <NumberInputField/>
@@ -59,7 +74,7 @@ const AddItem = (props) => {
             </FormControl>
           </Flex>
   
-          <FormControl id='expiration_date' mb="2%">
+          <FormControl id='expiration_date' mb="2%" isRequired>
             <FormLabel>Expiration date</FormLabel>
             <Input 
               placeholder="10/12/2021" 
@@ -69,8 +84,8 @@ const AddItem = (props) => {
           </FormControl>
   
           <Flex flexDirection='row' alignItems='center' mb="3%">
-            <FormControl id="height" marginEnd="2%">
-              <FormLabel>Height</FormLabel>
+            <FormControl id="height" marginEnd="2%" isRequired>
+              <FormLabel>Height (cm)</FormLabel>
               <SizeInputField 
                 defaultValue={0} 
                 min={0} 
@@ -80,8 +95,8 @@ const AddItem = (props) => {
               />
             </FormControl>
   
-            <FormControl id="lenght" marginEnd="2%">
-              <FormLabel>Length</FormLabel>
+            <FormControl id="lenght" marginEnd="2%" isRequired>
+              <FormLabel>Length (cm)</FormLabel>
               <SizeInputField
                 defaultValue={0}
                 min={0}
@@ -91,8 +106,8 @@ const AddItem = (props) => {
               />
             </FormControl>
   
-            <FormControl id="width" marginEnd="10%">
-              <FormLabel>Width</FormLabel>
+            <FormControl id="width" marginEnd="10%" isRequired>
+              <FormLabel>Width (cm)</FormLabel>
               <SizeInputField
                 defaultValue={0}
                 min={0}
@@ -102,8 +117,8 @@ const AddItem = (props) => {
               />
             </FormControl>
   
-            <FormControl id="Weight" alignSelf="end">
-              <FormLabel>Weight</FormLabel>
+            <FormControl id="Weight" alignSelf="end" isRequired>
+              <FormLabel>Weight (g)</FormLabel>
               <SizeInputField
                 defaultValue={0}
                 min={0}
@@ -118,6 +133,19 @@ const AddItem = (props) => {
           <Button colorScheme="teal" variant="solid" width="100%" onClick={addItemHandler}>
             Add Item
           </Button>
+          { formError === true && 
+            <Alert status='error'>
+              <AlertIcon />
+                All the fields are mandatory
+              </Alert>
+          }
+          { formSuccess === true && 
+            <Alert status='success'>
+              <AlertIcon />
+                Successfully added new item
+              </Alert>
+          }
+      
         </Flex>
     )
   }
